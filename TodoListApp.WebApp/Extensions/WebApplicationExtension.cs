04c1b732c452f04fa14028dca4f78ffa3334b378
teafr <tookyoumy7@gmail.com>
2025-06-10 00:21:13 +0300

    deleted complex routing

--- TodoListApp.WebApp/Extensions/WebApplicationExtension.cs
+++ TodoListApp.WebApp/Extensions/WebApplicationExtension.cs
@@ -4,36 +4,6 @@ public static class WebApplicationExtension
 {
     public static WebApplication AddRoutes(this WebApplication app)
     {
-        //_ = app.MapControllerRoute(
-        //    name: "search",
-        //    pattern: "Tasks/SearchTasks/{property:alpha}",
-        //    defaults: new { controller = "Tasks", action = "SearchTasks" });
-
-        //_ = app.MapControllerRoute(
-        //    name: "taskEdit",
-        //    pattern: "Tasks/Edit/{taskId:int}",
-        //    defaults: new { controller = "Tasks", action = "Edit" });
-
-        //_ = app.MapControllerRoute(
-        //    name: "taskCreate",
-        //    pattern: "Tasks/AddTask/{todoListId:int?}",
-        //    defaults: new { controller = "Tasks", action = "AddTask" });
-
-        //_ = app.MapControllerRoute(
-        //    name: "taskDetails",
-        //    pattern: "Tasks/Details/{taskId:int}",
-        //    defaults: new { controller = "Tasks", action = "Details" });
-
-        //_ = app.MapControllerRoute(
-        //    name: "sorting",
-        //    pattern: "Tasks/SortBy{sortBy:alpha}/Page{currentPage:int}",
-        //    defaults: new { controller = "Tasks", action = "Index", currentPage = 1, sortBy = "CreationDate" });
-
-        //_ = app.MapControllerRoute(
-        //    name: "todoListTasksPagination",
-        //    pattern: "TodoLists/{todoListId:int}/Tasks/Page{currentPage:int}",
-        //    defaults: new { controller = "TodoLists", action = "GetTasks", currentPage = 1 });
-
         _ = app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Tasks}/{action=Index}/{taskId:int?}",
